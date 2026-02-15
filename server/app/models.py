92fe790fe3feffdb782c799b4ad623aaba206914
Luca <auth@luca.cx>
2026-02-15 15:28:49 +0200

    Book models

--- server/app/models.py
+++ server/app/models.py
@@ -22,18 +22,23 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100, null=True)
     description = models.TextField(null=True)
-    file = models.FileField(upload_to='books/%Y/%m/')
+    file = models.FileField(upload_to="books/")
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
     # Access control
     uploaded_by = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name='uploaded_books'
+        related_name="uploaded_books"
     )
     is_public = models.BooleanField(default=False)
 
 
 class BookProgress(models.Model):
+    """
+    Model for book progress tracking.
+    The very existence of a row in this table means that the user has this book in their library.
+    """
     book = models.ForeignKey(Book, on_delete=models.PROTECT)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    sentence_last_read = models.IntegerField(default=0)
