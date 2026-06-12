f83329c8ef0bb0264a53d3b1943858b409e462ea
Luca <auth@luca.cx>
2026-06-12 20:57:10 +0200

    Allowed hosts

--- server/core/settings.py
+++ server/core/settings.py
@@ -32,7 +32,11 @@ if os.getenv("DEBUG") in ("False", False):
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "api.readablock.com",
+    "readablock.com",
+    "127.0.0.1",
+]
 
 
 # Application definition
