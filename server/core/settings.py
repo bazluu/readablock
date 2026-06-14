25098481a47066504be8b1a577f15259dc9a9961
Luca <auth@luca.cx>
2026-06-14 21:56:07 +0200

    psql

--- server/core/settings.py
+++ server/core/settings.py
@@ -107,8 +107,12 @@ if DEBUG:
 else:
     DATABASES = {
         "default": {
-            "ENGINE": "libsql_experimental.db.backend",
-            "URL": os.getenv("LIBSQL_URL"),
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("DB_NAME"),
+            "USER": os.getenv("DB_USER"),
+            "PASSWORD": os.getenv("DB_PASSWORD"),
+            "HOST": os.getenv("DB_HOST"),
+            "PORT": os.getenv("DB_PORT", "5432"),
         }
     }
 
