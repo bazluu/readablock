79b021dda77316a5c162a2178b7bd0651d0c1362
Luca <auth@luca.cx>
2026-08-17 18:03:25 +0200

    Change dev DB file

--- server/core/settings.py
+++ server/core/settings.py
@@ -103,7 +103,7 @@ if DEBUG:
     DATABASES = {
         "default": {
             "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "dev.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
         }
     }
 else:
