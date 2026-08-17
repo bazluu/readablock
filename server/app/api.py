a3f8925ca8b6a5c796192f005799ed44817d320a
Luca <auth@luca.cx>
2026-08-17 17:55:10 +0200

    Import cache

--- server/app/api.py
+++ server/app/api.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.core.cache import cache
 
 from ninja import NinjaAPI, File, Form
 from ninja.files import UploadedFile
