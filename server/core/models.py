c3015b7726149bb71c7badae9230102e616b1b8c
Luca <auth@luca.cx>
2026-09-22 19:19:43 +0200

    + Add field daily_word_goal to usermeta

--- server/core/models.py
+++ server/core/models.py
@@ -4,8 +4,18 @@ from django.utils import timezone
 
 
 class UserMeta(models.Model):
+    class DailyWordGoal(models.IntegerChoices):
+        W140 = 140
+        W550 = 550
+        W1370 = 1370
+        W2740 = 2740
+
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     verified = models.BooleanField(default=False)
+    daily_word_goal = models.IntegerField(
+        choices=DailyWordGoal.choices,
+        null=True
+    )
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
